a7344e8be5adafbaf274f02bac19ec20820b253d
beksay18 <saiitkulov.bektur@gmail.com>
2023-01-14 19:25:00 -0600

    entity,service,controller added

---
+++ src/main/java/com/beksay/dept/controller/DepartmentController.java
@@ -0,0 +1,30 @@
+package com.beksay.dept.controller;
+
+import com.beksay.dept.dto.DepartmentDto;
+import com.beksay.dept.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    //build save department REST API
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+
+    //get department REST API
+    @GetMapping("{department-code}")
+    public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode){
+        DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
+        return new ResponseEntity<>(departmentDto, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/beksay/dept/service/DepartmentService.java
@@ -0,0 +1,10 @@
+package com.beksay.dept.service;
+
+import com.beksay.dept.dto.DepartmentDto;
+
+public interface DepartmentService {
+
+    DepartmentDto saveDepartment(DepartmentDto departmentDto);
+
+    DepartmentDto getDepartmentByCode(String code);
+}
